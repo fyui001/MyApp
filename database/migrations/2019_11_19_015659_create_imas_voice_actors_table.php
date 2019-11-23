2f40f5502c4b8c2df32399c35854522cb23666dd
fyui001 <yun.cod@gmail.com>
2019-11-23 15:19:13 +0900

    マイグレーションの修正

--- database/migrations/2019_11_19_015659_create_imas_voice_actors_table.php
+++ database/migrations/2019_11_19_015659_create_imas_voice_actors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVoiceActorsTable extends Migration
+class CreateImasVoiceActorsTable extends Migration
 {
     /**
      * Run the migrations.
