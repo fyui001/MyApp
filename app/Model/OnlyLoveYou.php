b8a5984fc0d7daf1158b8f55e7b310abbc4e745d
fyui001 <yun.cod@gmail.com>
2019-11-26 01:53:37 +0900

    modelはこんな感じ

--- app/Model/OnlyLoveYou.php
+++ app/Model/OnlyLoveYou.php
@@ -8,8 +8,34 @@ class OnlyLoveYou extends Model
 {
     protected $table = 'only_love_you';
 
+    /**
+    * お前しか好きじゃないの全県取得
+    *
+    * @return array
+    */
     public function get() {
-        return $this->get() ?: false;
+
+        return $this->select('UserName', 'Content', 'Love', 'Guild', 'create_at')
+                    ->where(['del_flg' => 0])
+                    ->orderBy('id', 'desc')
+                    ->paginate(20);
+
+    }
+
+    /**
+     * ユーザー名、お前しか好きじゃない声優、サーバー名いずれかの検索結果
+     *
+     * @param string
+     * @return bool
+     */
+    public function search(string $keyword) {
+
+        return $this->select('UserName', 'Content', 'Love', 'Guild', 'create_at')
+                    ->where(['del_flg' => 0])
+                    ->where('UserName', 'like', "%{$keyword}%")
+                    ->where('Love', 'like', "%{$keyword}%")
+                    ->where('Guild', 'like', "%{$keyword}%")
+                    ->oderBy('id', 'desc')->paginate(20);
     }
 
 }
