a6fa5bd3b986128377d5cbae6ad58cc371f57108
fyui001 <yun.cod@gmail.com>
2019-11-26 03:05:15 +0900

    一覧取得と検索のコントローラー実装

--- app/Http/Controllers/OnlyLoveYouController.php
+++ app/Http/Controllers/OnlyLoveYouController.php
@@ -2,13 +2,57 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Model\OnlyLoveYou;
+use App\Http\Requests\OnlyLoveYouRequest;
 
 class OnlyLoveYouController extends Controller
 {
+
+    /**
+    * お前しか好きじゃない一覧取得
+    *
+    * @return array
+    */
     public function index() {
+
         $OnlyLoveYou = new OnlyLoveYou;
-        return $OnlyLoveYou->get() ?: 'データの取得に失敗';
+        $resultDatas = $OnlyLoveYou->get();
+
+        if ( $resultDatas->isNotEmpty() ) {
+            return [
+                'status' => true,
+                'resultData' => $resultDatas
+                ];
+        } else {
+            return [
+                'status' => false,
+                'msg' => 'エラーが発生しました'
+            ];
+        }
+
     }
+
+    /**
+     * 投稿者、声優、サーバーのいずれかで検索
+     *
+     * @param OnlyLoveYouRequest $request
+     * @return array
+     */
+    public function show(OnlyLoveYouRequest $request) {
+        $OnlyLoveYou = new OnlyLoveYou;
+        $searchResult = $OnlyLoveYou->search($request['searchKeyword']);
+
+        if ( $searchResult->isNotEmpty() ) {
+            return [
+                'status' => true,
+                'resultData' => $searchResult
+            ];
+        } else {
+            return [
+                'status' => true,
+                'resultData' => $OnlyLoveYou->get()
+            ];
+        }
+    }
+
 }
